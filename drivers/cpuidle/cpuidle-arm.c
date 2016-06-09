560dae99ffdcf6488601a25e136a6639e6f2307d
Edward Cragg <edward.cragg@codethink.co.uk>
2016-06-09 16:54:29 +0100

    DEBUG: Overide CPU suspend state index to 0

--- drivers/cpuidle/cpuidle-arm.c
+++ drivers/cpuidle/cpuidle-arm.c
@@ -50,7 +50,8 @@ static int arm_enter_idle_state(struct cpuidle_device *dev,
 		 * call the CPU ops suspend protocol with idle index as a
 		 * parameter.
 		 */
-		arm_cpuidle_suspend(idx);
+		printk('Overriding suspend state index %d to 0', idx);
+		arm_cpuidle_suspend(0);
 
 		cpu_pm_exit();
 	}
